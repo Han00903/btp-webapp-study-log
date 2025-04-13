022f16cb077cb3176b78ddf1a22e0213ad05d158
은정 한 <hanjung25@naver.com>
2025-04-13 20:08:35 +0000

    push

--- app/component/request/controller/app.controller.js
+++ app/component/request/controller/app.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
-        "sap/ui/core/mvc/Controller"
-     ], function (Controller) {
-        "use strict";
-        return Controller.extend("project1.controller.app", {
-        });
-     });
-    
+       "sap/ui/core/mvc/Controller"
+    ], function (Controller) {
+       "use strict";
+       return Controller.extend("project1.component.request.controller.app", {
+   
+       });
+    });
+   
